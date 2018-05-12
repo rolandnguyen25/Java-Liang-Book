626ad08bcf0eeb3175a45b1a375e583c6cee964c
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-12 14:39:36 -0500

    Exercise 1.2 Display Java messages 5 times

---
+++ src/Chap1/Exercise_1_2_Display_Java_Messages_5_Times.java
@@ -0,0 +1,16 @@
+package Chap1;
+
+/**
+ * (Display five messages) Write a program that displays Welcome to Java five times.
+ */
+
+public class Exercise_1_2_Display_Java_Messages_5_Times {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+        System.out.println("Welcome to Java");
+    }
+}
