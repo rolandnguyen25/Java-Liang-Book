0ff944a904a1b8cb4af758f35272fe02221dd535
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-13 06:39:06 -0500

    Exercise 1.3 Display a Pattern

---
+++ src/Chap1/Exercise_1_3_Display_A_Pattern.java
@@ -0,0 +1,19 @@
+package Chap1;
+
+/**
+ * (Display a pattern) Write a program that displays the following pattern:
+ J A V V A
+ J A A V V A A
+ J J AAAAA V V AAAAA
+ J J A A V A A
+ */
+
+public class Exercise_1_3_Display_A_Pattern {
+
+    public static void main(String[] args) {
+        System.out.println("     J      A      V      V     A");
+        System.out.println("     J     A A      V    V     A A");
+        System.out.println("J    J    AAAAA      V  V     AAAAA");
+        System.out.println(" J J     A     A      V      A     A");
+    }
+}
