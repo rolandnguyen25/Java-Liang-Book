7e277eb8de05e5b81894a8d9954233605a3d6d75
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-13 11:46:47 -0500

    Exercise 1.8 Area and Perimeter of Circle

---
+++ src/Chap1/Exercise_1_8_Area_Perimeter_Of_Circle.java
@@ -0,0 +1,18 @@
+package Chap1;
+
+/**
+ * (Area and perimeter of a circle) Write a program that displays the area and perimeter
+ of a circle that has a radius of 5.5 using the following formula:
+ perimeter = 2 * radius * PI
+ area = radius * radius * PI
+ */
+
+public class Exercise_1_8_Area_Perimeter_Of_Circle {
+
+    public static void main(String[] args) {
+        System.out.print("Perimeter of circle with radius of 5.5");
+        System.out.print(2 * 5.5 * 3.14);
+        System.out.print("\nArea of circle with radius of 5.5");
+        System.out.print(5.5 * 5.5 * 3.14);
+    }
+}
