a3649f8aa39ee339a7e52666fa36193dff30d0d2
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-13 10:07:36 -0500

    Exercise 1.7 Approximate PI

---
+++ src/Chap1/Exercise_1_7_Approximate_PI.java
@@ -0,0 +1,15 @@
+package Chap1;
+
+/**
+ * (Approximate) pi can be computed using the following formula:
+ *
+ *
+ */
+
+public class Exercise_1_7_Approximate_PI {
+
+    public static void main(String[] args) {
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11));
+        System.out.println(4 * (1.0 - 1.0/3 + 1.0/5 - 1.0/7 + 1.0/9 - 1.0/11 + 1.0/13));
+    }
+}
