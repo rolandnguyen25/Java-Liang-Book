37c58b00b085865086818d7faca6a74827bc1607
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-13 07:13:18 -0500

    Exercise 1.6 Summation of a Series

---
+++ src/Chap1/Exercise_1_6_Summation_of_a_Series.java
@@ -0,0 +1,13 @@
+package Chap1;
+
+/**
+ * (Summation of a series) Write a program that displays the result of
+ 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9.
+ */
+
+public class Exercise_1_6_Summation_of_a_Series {
+
+    public static void main(String[] args) {
+        System.out.println(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9);
+    }
+}
