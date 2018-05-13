e2bf3ad1b0b75c93ad7540a1a83c3630b95c0005
Roland Nguyen <rolandnguyen25@gmail.com>
2018-05-13 07:11:29 -0500

    Exercise 1.5 Compute Expressions

---
+++ src/Chap1/Exercise_1_5_Compute_Expressions.java
@@ -0,0 +1,14 @@
+package Chap1;
+
+/**
+ * (Compute expressions) Write a program that displays the result of
+ 9.5 * 4.5 - 2.5 * 3
+ 45.5 - 3.5
+ */
+
+public class Exercise_1_5_Compute_Expressions {
+
+    public static void main(String[] args) {
+        System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+    }
+}
